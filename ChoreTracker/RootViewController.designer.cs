a37ab86dc1b66c9e0a5d193b34822ce477101795
HeidivdWest <heidi@lazymanjoe.com>
2017-04-27 14:53:05 +0200

    Added icons and launch screen

--- ChoreTracker/RootViewController.designer.cs
+++ ChoreTracker/RootViewController.designer.cs
@@ -1,13 +1,20 @@
-﻿using Foundation;
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace ChoreTracker
 {
-	[Register("RootViewController")]
-	partial class RootViewController
-	{
-		void ReleaseDesignerOutlets()
-		{
-		}
-	}
+    [Register ("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
 }
